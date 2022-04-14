9dd7a296fceda2ba68d338a813135f52b1e19997
dev <dev@mostafas-MacBook-Pro.fritz.box>
2022-04-15 01:53:43 +0200

    update

--- src/App.js
+++ src/App.js
@@ -23,9 +23,7 @@ const App = () => {
         <img src={logo} className="App-logo" alt="logo" />
         <p>{message}</p>
         <p>Change me!</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
         
       </header>
     </div>
