05837e19ade9a72ad9bcab49553b9d917294ece1
dev <dev@mostafas-MacBook-Pro.fritz.box>
2022-04-15 01:58:10 +0200

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
@@ -19,8 +18,8 @@ const App = () => {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header >
+        
         <p>{message}</p>
         <p>Change me!</p>
         hhhhh
